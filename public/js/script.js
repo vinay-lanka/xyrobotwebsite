feaca96263c1edf76554f8757cb4034aad04e1bc
vinay-lanka <lanka.vinay2017@vitstudent.ac.in>
2019-11-28 00:54:35 +0530

    Finished Frontend

---
+++ public/js/script.js
@@ -0,0 +1,37 @@
+var app = angular.module('robotApp', []);
+app.controller('pointsCtrl', function($scope, $http) {
+  $scope.showCoordinateCardVar = false;
+  $scope.showPointsCardVar = true;
+  $scope.sendPoints = function(number) {
+    console.log(number);
+    $http.post("/setpoints", {points:number})
+    .then(function(response) {
+        $scope.points = response.data;
+        var range = [];
+        var x=0,y=0;
+        for(var i=1;i<=response.data;i++) {
+          range.push({i,x,y});
+        }
+        $scope.range = range;
+        console.log(response.data);
+        $scope.showCoordinateCardVar = true;
+        $scope.showPointsCardVar = false;
+    });
+  }
+
+  $scope.back = function(number) {
+    console.log($scope.range)
+    $scope.showCoordinateCardVar = false;
+    $scope.showPointsCardVar = true;
+  }
+
+  $scope.submit = function(number) {
+    console.log($scope.range)
+    $http.post("/submit", $scope.range)
+    .then(function(response) {
+      console.log(response);
+    });
+    // $scope.showCoordinateCardVar = false;
+    // $scope.showPointsCardVar = true;
+  }
+});
--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 app.use(express.static(__dirname + '/public/'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) =>{
     res.sendFile('/public/pages/index.html', {'root': './'});
+})
+
+app.post('/setpoints', (req, res) =>{
+    console.log(req.body.points);
+    res.send(req.body.points);
+})
+
+app.post('/submit', (req, res) =>{
+    console.log(req.body);
+    res.send("Submitted");
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
